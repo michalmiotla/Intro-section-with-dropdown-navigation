3a750fbfee46a0200b34bd66e3f86afa321a44f2
Michał Miotła <miotlamichal@gmail.com>
2023-06-09 19:36:42 +0200

    working with grid desktop display

--- intro section with dropdown navigation/js/script.js
+++ intro section with dropdown navigation/js/script.js
@@ -5,6 +5,11 @@ const featuresDiv = document.querySelector('.features')
 const companyLink = document.querySelector('.company-link')
 const companyDiv = document.querySelector('.company')
 
+const featuresDesktopLink = document.querySelector('.features-link-desktop')
+const featuresDesktopDiv = document.querySelector('.features-desktop')
+const companyDesktopLink = document.querySelector('.company-link-desktop')
+const companyDesktopDiv = document.querySelector('.company-desktop')
+
 const showNav = () => {
 	mobileNav.classList.toggle('show')
 }
@@ -21,6 +26,20 @@ const showCompany = () => {
 	}
 }
 
+const showDesktopFeatures = () => {
+	if ((featuresDesktopDiv.style.display = 'none')) {
+		featuresDesktopDiv.style.display = 'flex'
+	}
+}
+
+const showDesktopCompany = () => {
+	if ((companyDesktopDiv.style.display = 'none')) {
+		companyDesktopDiv.style.display = 'flex'
+	}
+}
+
 burgerBtn.addEventListener('click', showNav)
 featuresLink.addEventListener('click', showFeatures)
 companyLink.addEventListener('click', showCompany)
+featuresDesktopLink.addEventListener('click', showDesktopFeatures)
+companyDesktopLink.addEventListener('click', showDesktopCompany)
